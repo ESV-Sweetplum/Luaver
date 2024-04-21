b2d4b0f0cac1d4c36725a677278c0ff97543c89d
ESV Sweetplum <sweetplumquaver@gmail.com>
2024-04-21 13:52:32 -0700

    Update main.lua

--- src/pages/main.lua
+++ src/pages/main.lua
@@ -1,5 +1,12 @@
 function main(params)
-    imgui.Text("I am the main point")
+    Text("Welcome to Luaver!")
 
-    Button("go to foo page", function() router.push("foo") end)
+    Text("Here are examples of built-in globals:")
+    Text("Selected StartTime: ", Region.startTime, "Selected EndTime: ", Region.endTime)
+    Button("Detect selected objects within selection region", function() getSelectedObjects() end)
+
+    Text("Detected Notes: ", #selected.notes) -- Either use the selected global,
+    Text("Detected Lines: ", #SelectedLines)  -- or use Selected___ for currently selected objects.
+    Text("Detected SVs: ", #selected.svs)     -- Both selected and Selected____ are supported.
+    Text("Detected Bookmarks: ", #SelectedBookmarks)
 end

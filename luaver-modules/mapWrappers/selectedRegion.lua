cf822d0899c605a8a6a90e43f2822a9bcee48542
ESV Sweetplum <sweetplumquaver@gmail.com>
2024-04-21 14:17:18 -0700

    Added table.property feature

--- luaver-modules/mapWrappers/selectedRegion.lua
+++ luaver-modules/mapWrappers/selectedRegion.lua
@@ -10,15 +10,16 @@ Region = {
 ---Gets the start and end times of the selected region.
 ---@return Region
 function getSelectedRegion()
-    local times = {}
-
     if (#state.SelectedHitObjects == 0) then return { startTime = -1e304, endTime = -1e304 } end
-    if (#state.SelectedHitObjects == 1) then return { startTime = state.SelectedHitObjects[1].StartTime, endTime = state
-        .SelectedHitObjects[1].StartTime } end
-
-    for _, v in pairs(state.SelectedHitObjects) do
-        table.insert(times, v.StartTime)
+    if (#state.SelectedHitObjects == 1) then
+        return {
+            startTime = state.SelectedHitObjects[1].StartTime,
+            endTime = state
+                .SelectedHitObjects[1].StartTime
+        }
     end
+
+    local times = table.property(state.SelectedHitObjects, "StartTime")
 
     Region = {
         startTime = math.min(table.unpack(times)),

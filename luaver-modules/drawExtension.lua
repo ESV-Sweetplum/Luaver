3689847ce96e46c096bde4c14e07f80113e98309
ESV Sweetplum <sweetplumquaver@gmail.com>
2024-04-22 21:26:11 -0700

    Added comments to drawExtension

--- luaver-modules/drawExtension.lua
+++ luaver-modules/drawExtension.lua
@@ -1,6 +1,6 @@
 function drawExtension()
-    routerDictionary[routerHistory[#routerHistory]]()
-    getSelectedObjectsState()
-    inputCounter = 1
-    refreshSZ()
+    routerDictionary[routerHistory[#routerHistory]]() -- Handles page rendering (routerDictionary is generated on transpile)
+    getSelectedObjectsState()                         -- Handles object selection and auto-cache (selected and region globals)
+    inputCounter = 1                                  -- Ensures that all input keys remain the same
+    refreshSZ()                                       -- Establishes sz global
 end

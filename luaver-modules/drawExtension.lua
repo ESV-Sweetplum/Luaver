7690b5be75fafebbfd124e2109a46321e4cb9388
ESV-Sweetplum <159661878+ESV-Sweetplum@users.noreply.github.com>
2024-06-01 07:49:02 +0800

    Temporarily move cache counter above page renderer

--- luaver-modules/drawExtension.lua
+++ luaver-modules/drawExtension.lua
@@ -2,6 +2,6 @@ function drawExtension()
     routerDictionary[routerHistory[#routerHistory]](paramHistory[#paramHistory]) -- Handles page rendering (routerDictionary is generated on transpile)
     getSelectedObjectsState()                                                    -- Handles object selection and auto-cache (selected and region globals)
     refreshSZ()                                                                  -- Establishes sz global
-    cacheCounter = 0                                                             -- Reset cache counter
+    -- cacheCounter = 0                                                             -- Reset cache counter
     if (state.GetValue("loaded") ~= true) then onLoad() end                      -- Run load function on load
 end
--- src/_layout.lua
+++ src/_layout.lua
@@ -1,6 +1,8 @@
 function draw()
     imgui.Begin("Luaver-Testing-Environment")
 
+    cacheCounter = 0
+
     -- SHARED ELEMENTS ACROSS ALL PAGES GO BEFORE IMGUI.END()
 
     RouterTabs({ "Info", "Globals", "Inputs" }, { "main", "globalsDemo", "inputsDemo" })

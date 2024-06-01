d40f269f9a06e4a31823b73495b25ebfe55179f6
ESV-Sweetplum <159661878+ESV-Sweetplum@users.noreply.github.com>
2024-06-01 08:11:43 +0800

    Add nil condition to prevent initial truth

--- luaver-modules/state/hasChanged.lua
+++ luaver-modules/state/hasChanged.lua
@@ -7,6 +7,8 @@ function hasChanged(var)
   local oldValue = state.GetValue("cache_" .. cacheCounter)
   local returnCondition = (oldValue ~= var)
 
+  if (oldValue == nil) then returnCondition = false end
+
   state.SetValue("cache_" .. cacheCounter, var)
   cacheCounter = cacheCounter + 1
   return returnCondition

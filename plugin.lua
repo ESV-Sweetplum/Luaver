388042296d56ca305abafc62f83699b8f0e2d224
plummyyummy <sweetplumquaver@gmail.com>
2026-03-18 14:03:20 -0700

    allow unpacked/packed ver of quinsight

--- plugin.lua
+++ plugin.lua
@@ -1,4 +1,5 @@
 math.randomseed(os.time())
+imgui_disable_vector_packing=true
 PLUGIN_NAME="LuaverTest";PLUGIN_VERSION="1.0.0";PLUGIN_AUTHOR="plummyyummy";PLUGIN_DESCRIPTION="Nothing Special"
 function awake()
 end

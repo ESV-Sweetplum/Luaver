aca226331a0ec550390e4464a6debe1d5d23b5b2
plummyyummy <sweetplumquaver@gmail.com>
2026-03-18 22:24:08 -0700

    fix minoir bugs

--- src/_processors/removeRequireAndMeta.file.ts
+++ src/_processors/removeRequireAndMeta.file.ts
@@ -1,5 +1,7 @@
 export default function removeRequireAndMeta(output: string[]) {
     return output.filter((line) => {
-        !/^require\(('").+?('")\)/.test(line) && !/^\s*---@meta/.test(line);
+        return (
+            !/^require\(('").+?('")\)/.test(line) && !/^\s*---@meta/.test(line)
+        );
     });
 }

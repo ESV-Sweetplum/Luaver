86f589c36c5d38fecfd0fb5c02e9070e3a571645
plummyyummy <sweetplumquaver@gmail.com>
2026-03-18 22:28:41 -0700

    apply require plugin

--- src/_processors/removeRequireAndMeta.file.ts
+++ src/_processors/removeRequireAndMeta.file.ts
@@ -1,7 +1,12 @@
-export default function removeRequireAndMeta(output: string[]) {
-    return output.filter((line) => {
-        return (
-            !/^require\(('").+?('")\)/.test(line) && !/^\s*---@meta/.test(line)
-        );
-    });
+import LuaverConfig from '../interfaces/luaverConfig';
+
+export default function removeRequireAndMeta(
+    input: string[],
+    config: LuaverConfig
+) {
+    let output = input
+        .filter((l) => l && !l.includes('require('))
+        .join(config.lineSeparator);
+
+    output = output.replaceAll(/---@meta [a-zA-Z0-9\-]+\n/g, '');
 }

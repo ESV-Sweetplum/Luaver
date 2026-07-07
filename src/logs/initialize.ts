682b56e6ed7a178a2326c88d216b76e8a778ace9
plummyyummy <sweetplumquaver@gmail.com>
2026-07-06 21:45:11 -0700

    edit log based output

--- src/logs/initialize.ts
+++ src/logs/initialize.ts
@@ -6,9 +6,11 @@ let output: string[] = [];
 const logs = {
     add: (msg: string) => output.push(msg),
     finalize: () => {
-        const logFolder = getAbsolutePath(`Luaver/logs`);
+        const logFolder = getAbsolutePath(`logs/Luaver`);
         if (!fs.existsSync(logFolder)) fs.mkdirSync(logFolder);
-        const latestPath = getAbsolutePath(`Luaver/logs/latest.log`);
+        const curPath = getAbsolutePath(`logs/Luaver/${new Date().toString()}`);
+        const latestPath = getAbsolutePath(`logs/Luaver/latest.log`);
+        fs.writeFileSync(curPath, output.join('\n'));
         fs.writeFileSync(latestPath, output.join('\n'));
     },
     reset: () => (output = []),
